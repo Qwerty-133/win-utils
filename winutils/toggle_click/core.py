5aa28495ef8e7e13e3fd09b018d96d14aa28edcc
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2024-02-12 17:37:59 +0530

    Make all imports lazy

--- winutils/toggle_click/core.py
+++ winutils/toggle_click/core.py
@@ -1,14 +1,13 @@
 """The core functionality for toggling the primary mouse button."""
-import win32api
-import win32con
-import ctypes
-from winutils._helpers import toast
-
 ICON_NAME = "cursor.ico"
 
 
 def swap_mouse_buttons() -> int:
     """Swap the primary mouse button and return the current primary button."""
+    import win32api
+    import win32con
+    import ctypes
+
     current_state = win32api.GetSystemMetrics(win32con.SM_SWAPBUTTON)
     new_state = 1 - current_state
     ctypes.windll.user32.SwapMouseButton(new_state)
@@ -17,8 +16,9 @@ def swap_mouse_buttons() -> int:
 
 def notify_state(state: int) -> None:
     """Show a toast for the current primary mouse button state."""
-    state_repr = "left" if state == 0 else "right"
+    from winutils._helpers import toast
 
+    state_repr = "left" if state == 0 else "right"
     toast.show_toast(
         "Mouse buttons swapped",
         f"The primary mouse button is now the {state_repr} button.",
